9b11ab6e2c083e3c718529d57921386e1e5ecb9a
readonebm <banuridwanmubarok@gmail.com>
2024-02-12 14:34:09 +0700

    repo test logic banu

---
+++ testlogic-banurm/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace testlogic_banurm
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string procVowel(string param)
+            {
+                string paraVowel = param.ToLower();
+                //string[] paramVowels = paraVowel.Split(' ');
+
+
+                char[] inpParams = paraVowel.ToCharArray();
+
+                List<char> vowels = new List<char>();
+
+                string result = "";
+
+                foreach (char s in inpParams)
+                {
+                    
+                    if (s == 'a' || s == 'i' || s == 'u' || s == 'e' || s == 'o')
+                    { 
+                        vowels.Add(s);
+                    }
+                }
+
+                foreach (char s in vowels.OrderBy(i => i))
+                {
+           
+                    result += s;
+                }
+
+                return result;
+            }
+
+            string procConsonant(string param)
+            {
+                string paraCons = param.ToLower();
+                char[] inpParams = paraCons.ToCharArray();
+
+                List<char> vowels = new List<char>();
+
+                string result = "";
+
+                foreach (char s in inpParams)
+                {
+                    if (s == 'a' || s == 'i' || s == 'u' || s == 'e' || s == 'o')
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        vowels.Add(s);
+                    }
+                }
+
+                foreach (char s in vowels.OrderByDescending(i => i))
+                {
+
+                    result += s;
+                }
+
+                return result;
+            }
+
+
+            Console.Write("Input one line of words (S) : ");
+            string input = Console.ReadLine();
+            //string input = "Sample Case";
+
+            string charVowel = procVowel(input);
+            string charConsonant = procConsonant(input);
+
+            Console.WriteLine("Vowel Characters : ");
+            Console.WriteLine(charVowel);
+            Console.WriteLine("Consonant Characters : ");
+            Console.WriteLine(charConsonant);
+        }
+    }
+}
---
+++ testlogic2-banurm/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+
+namespace testlogic2_banurm
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the number of families: ");
+            int inpNum = Convert.ToInt32(Console.ReadLine());
+            //int inpNum = 5;
+
+            Console.WriteLine("Input the number of members in the family\r\n( separated by a space) :");
+            string membFam = Console.ReadLine();
+            //string membFam = "1 2 4 3 3";
+            //string membFam = "2 3 4 4 2 1 3 1";
+
+
+            string[] memberFams = membFam.Split(' ').ToArray();
+            if (memberFams.Length < inpNum)
+            {
+                Console.WriteLine("Input must be equal with count of family");
+            }
+            else
+            {
+                int[] membersFams = Array.ConvertAll(memberFams, Int32.Parse);
+
+                int totalMem = 0;
+                int minPass = 4;
+                int minBus = 0;
+
+                foreach (int s in membersFams)
+                {
+                    totalMem += s;
+                }
+
+                int sisaBagi = totalMem % minPass;
+                if(sisaBagi == 0) {
+                    minBus = totalMem / minPass;
+                }
+                else
+                {
+                    minBus = (totalMem - sisaBagi) / minPass;
+
+                    if(sisaBagi <= 4)
+                    {
+                        minBus ++;
+                    }
+                }
+
+                Console.WriteLine($"Minimum bus required is: {minBus}");
+            }
+
+            
+            
+
+
+
+        }
+    }
+}
